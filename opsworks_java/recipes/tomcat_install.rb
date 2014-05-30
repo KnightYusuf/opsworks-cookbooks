a5848e78eb2fe322fbd07f788235051c938be509
KnightYusuf <naitsoft@gmail.com>
2014-05-30 19:27:53 +0200

    Update tomcat_install.rb

--- opsworks_java/recipes/tomcat_install.rb
+++ opsworks_java/recipes/tomcat_install.rb
@@ -15,13 +15,11 @@ link ::File.join(node['opsworks_java']['tomcat']['lib_dir'], node['opsworks_java
 end
 
 execute "import_redis_jar" do
-command "wget https://github.com/downloads/jcoleman/tomcat-redis-session-manager/tomcat-redis-session-manager-1.2-tomcat-7.jar
--P #{node['opsworks_java']['tomcat']['lib_dir']}"
+  command "wget https://github.com/downloads/jcoleman/tomcat-redis-session-manager/tomcat-redis-session-manager-1.2-tomcat-7.jar -P #{node['opsworks_java']['tomcat']['lib_dir']}"
 end
 
 execute "import_jedis_jar" do
-command "wget http://repo1.maven.org/maven2/redis/clients/jedis/2.4.2/jedis-2.4.2.jar
--P #{node['opsworks_java']['tomcat']['lib_dir']}"
+  command "wget http://repo1.maven.org/maven2/redis/clients/jedis/2.4.2/jedis-2.4.2.jar -P #{node['opsworks_java']['tomcat']['lib_dir']}"
 end
 
 # remove the ROOT webapp, if it got installed by default

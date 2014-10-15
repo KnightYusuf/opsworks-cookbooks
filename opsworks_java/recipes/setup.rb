b4cb8ee0b6d73da30a6c431b640ed38ca702ea44
KnightYusuf <naitsoft@gmail.com>
2014-10-15 12:59:22 +0100

    Update setup.rb

--- opsworks_java/recipes/setup.rb
+++ opsworks_java/recipes/setup.rb
@@ -1,2 +1,6 @@
 #include_recipe 'opsworks_java::tomcat_config'
+execute 'owner tomcat' do
+2	  command 'chown -R ec2-user /opt/tomcat'
+execute 'start tomcat' do
+2	  command '/opt/tomcat/bin/./shtartup.sh'
 
